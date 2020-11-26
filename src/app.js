52f2e943a4fd10adca843d6f17821087699a77e8
wanghailing <wanghailing@mail.fantuan.ca>
2020-11-26 12:47:41 +0800

    refactor: 调整目录结构

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
-// error handler
+// error handler 在页面上面显示
 onerror(app)
 
 // middlewares
@@ -17,9 +17,11 @@ app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
+// 可以在控制台里面打印一些日志
 app.use(logger())
+// 把public里面的目录注册为静态化了
 app.use(require('koa-static')(__dirname + '/public'))
-
+//注册ejs的文件，不注册用不了ejs的语法
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
@@ -36,7 +38,7 @@ app.use(async (ctx, next) => {
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
 
-// error-handling
+// error-handling 在控制台里面打印
 app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 });
