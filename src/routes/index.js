1630f74c264bb8d4cae38e46f6d56a0305e45a4f
wanghailing <wanghailing@mail.fantuan.ca>
2020-12-18 17:39:37 +0800

    feat:测试sequelize的链接

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,33 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    msg: 'eeeee',
+    isme: true,
+    list:[
+      {
+        id: '2313',
+        component: '这是内容简介11'
+      },
+
+      {
+        id: '2313',
+        component: '这是内容简介22'
+      }
+    ]
   })
 })
 
 router.get('/string', async (ctx, next) => {
   ctx.body = 'koa2 string'
+})
+
+router.get('/profile/:username', async (ctx, next) => {
+  const {username} = ctx.params
+  ctx.body ={
+    title: 'this is a profile page',
+    username
+  }
 })
 
 router.get('/json', async (ctx, next) => {
