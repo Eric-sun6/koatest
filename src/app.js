2d277b9352c45f05bbf3192f5302bb281a6604f8
wanghailing <wanghailing@mail.fantuan.ca>
2021-02-26 11:04:23 +0800

    feat:404路由的组件和路由的添加

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ const logger = require('koa-logger')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
-
+const errorViewRouter = require('./routes/viewRoute/error')
 // error handler 在页面上面显示
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+    enableTypes:['json', 'form', 'text']
 }))
 app.use(json())
 // 可以在控制台里面打印一些日志
@@ -23,24 +23,28 @@ app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 //注册ejs的文件，不注册用不了ejs的语法
 app.use(views(__dirname + '/views', {
-  extension: 'ejs'
+    extension: 'ejs'
 }))
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+
+    const start = new Date()
+    await next()
+    const ms = new Date() - start
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+
 })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
-
+app.use(errorViewRouter.routes(), errorViewRouter.allowedMethods())
 // error-handling 在控制台里面打印
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+
+    console.error('server error', err, ctx)
+
 });
 
 module.exports = app
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,40 +1,51 @@
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render('index', {
-    title: 'Hello Koa 2!',
-    msg: 'eeeee',
-    isme: true,
-    list:[
-      {
-        id: '2313',
-        component: '这是内容简介11'
-      },
 
-      {
-        id: '2313',
-        component: '这是内容简介22'
-      }
-    ]
-  })
+    // debugger
+    // console.log(9999)
+    // 添加debugger然后在Chrome/inspect 的console中调试
+    await ctx.render('index', {
+        title: 'Hello Koa 2!',
+        msg: 'eeeee',
+        isme: true,
+        list:[
+            {
+                id: '2313',
+                component: '这是内容简介11'
+            },
+
+            {
+                id: '2313',
+                component: '这是内容简介22'
+            }
+        ]
+    })
+
 })
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
+
+    ctx.body = 'koa2 string'
+
 })
 
 router.get('/profile/:username', async (ctx, next) => {
-  const {username} = ctx.params
-  ctx.body ={
-    title: 'this is a profile page',
-    username
-  }
+
+    const {username} = ctx.params
+    ctx.body ={
+        title: 'this is a profile page',
+        username
+    }
+
 })
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
-  }
+
+    ctx.body = {
+        title: 'koa2 json'
+    }
+
 })
 
 module.exports = router
